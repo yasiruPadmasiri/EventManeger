26532d3cca250e46472e5a417ec1af0e5171279d
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-12-24 19:11:57 +0530

    undo all

--- EventManeger/src/main/java/com/EventManeger/EventManeger/Controllers/CountryController.java
+++ EventManeger/src/main/java/com/EventManeger/EventManeger/Controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +19,7 @@ public class CountryController {
     private CountryService countryService;
 
 
-    
+
     @GetMapping("/countries")          //link eke wetena nama
     public String GetCountries(Model model) {
 
@@ -37,32 +38,27 @@ public class CountryController {
 
 
 //    @RequestMapping("/countries/update")
-//    public String GetCountriesFind() {
-////    public String GetCountriesFind(Model models,@RequestParam int id) {
-////
-////        Optional<Country> ScountryList=countryService.findById(id);
-////
-////        models.addAttribute("countries",ScountryList);
-//        return "countryUpdate";              //link eke  namata anuwa pennan oona html file eke .html kyna tika nathuwa
-////    }
-
-
-
-
-//    @RequestMapping(value = "/countries/delete" ,method = {RequestMethod.DELETE,RequestMethod.GET})
-//    public String delete(Integer id){
-//        countryService.delete(id);
-//        return  "redirect:/countries";
+//    @PutMapping
+//    public String GetCountriesFind(Model models,int id) {
 //
 //    }
 //    @RequestMapping("/countries/findById")
 //    @ResponseBody
 //    public Optional<Country> findById(Model models,int id){
 //
-//        return countryService.findById(id) ;
-//
+//        models.addAttribute("countrie",ScountryList);
+//        return "country";              //link eke  namata anuwa pennan oona html file eke .html kyna tika nathuwa
 //    }
+    @RequestMapping(value = "/countries/delete" ,method = {RequestMethod.DELETE,RequestMethod.GET})
+    public String delete(Integer id){
+        countryService.delete(id);
+        return  "redirect:/countries";
 
-
+    }
+    @RequestMapping("/countries/findById")
+    @ResponseBody
+    public Optional<Country> findById(int id){
+        return countryService.findById(id);
+    }
 
 
--- EventManeger/src/main/resources/static/js/country.js
+++ EventManeger/src/main/resources/static/js/country.js
@@ -1,22 +1,6 @@
 $('doucument').ready(function (){
     $('table editButton').on('click',function (event){
         $('editModal').modal();
-        event.preventDefault();
-        var href=$(this).attr('href');
-
-
-        $.get(href,function (country,status){
-
-            $('#idEdit').va(country.id);
-            $('#descriptionEdit').va(country.description);
-            $('#codeEdit').va(country.code);
-            $('#capitalEdit').va(country.capital);
-            $('#continentEdit').va(country.continent);
-            $('#nationalityEdit').va(country.nationality);
-
-
-        });
-        $('#editModal').modal();
 
     })
 })
